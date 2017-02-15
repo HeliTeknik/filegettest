f37389efee55679dee63a5dfdee0792da47cee65
HeliTeknik <HeliTeknik@users.noreply.github.com>
2017-02-15 19:12:12 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,28 +1,18 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+/*
+<?
 $content = file_get_contents('http://www.sozcu.com.tr/kategori/yazarlar/');
 echo $content;
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+*/
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+
+
+<?php
+require 'scraperwiki.php';
+// A scraper for airline information from PHL.org
+require 'scraperwiki/simple_html_dom.php'; 
+$html_content = scraperwiki::scrape("http://www.sozcu.com.tr/kategori/yazarlar"); 
+$html = str_get_html($html_content);
+scraperwiki::sqliteexecute("drop table if exists swdata"); 
+scraperwiki::save_var('html',  $html);
 ?>

0ea28047ef96a45d4244cd74e124340283b4c18a
HeliTeknik <HeliTeknik@users.noreply.github.com>
2017-02-15 18:53:49 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,7 +4,8 @@
 
 // require 'scraperwiki.php';
 // require 'scraperwiki/simple_html_dom.php';
-//
+$content = file_get_contents('www.sozcu.com.tr/kategori/yazarlar/');
+echo $content;
 // // Read in a page
 // $html = scraperwiki::scrape("http://foo.com");
 //

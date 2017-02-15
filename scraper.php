95c3f7c5593e3da6866ea468742bab0c02db0224
HeliTeknik <HeliTeknik@users.noreply.github.com>
2017-02-15 19:18:48 +0200

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,17 +1,18 @@
-//
-//
-//$content = file_get_contents('http://www.sozcu.com.tr/kategori/yazarlar/');
-//echo $content;
-//
-
+<?php
+require 'scraperwiki.php';
+    require 'scraperwiki/simple_html_dom.php';
+?>
 
 
 <?php
-require 'scraperwiki.php';
-// A scraper for airline information from PHL.org
-require 'scraperwiki/simple_html_dom.php'; 
-$html_content = scraperwiki::scrape("http://www.sozcu.com.tr/kategori/yazarlar"); 
-$html = str_get_html($html_content);
-scraperwiki::sqliteexecute("drop table if exists swdata"); 
-scraperwiki::save_var('html',  $html);
+$url="http://www.sozcu.com.tr/kategori/yazarlar";
+$html = scraperWiki::scrape( $url );
+$dom = new simple_html_dom();
+$dom->load($html);
+
+$record = array(
+            'html' => $tds[0]->plaintext 
+        );
+ scraperwiki::save( array('html'), $record )
+
 ?>
